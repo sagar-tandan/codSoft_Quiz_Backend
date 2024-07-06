52a822b80a9c9a1e0ede4befed35b88b988c71ad
Sagar Tandan <076bct072@ioepc.edu.np>
2024-07-06 19:09:19 +0545

    quizcontroller and routes tested and completed

--- controllers/quizControllers.js
+++ controllers/quizControllers.js
@@ -124,4 +124,140 @@ const editQuiz = async (req, res) => {
   }
 };
 
-module.exports = { addQuiz, getAllQuizs, getQuizById, editQuiz };
+const deleteQuiz = async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.body.userid });
+    if (user.isAdmin) {
+      const quiz = await Quiz.findOne({ _id: req.params.id });
+      if (quiz) {
+        quiz.delete();
+        res.send({
+          message: "Selected quiz deleted successfully.",
+          data: null,
+          success: true,
+        });
+      } else {
+        res.send({
+          message: "Quiz doesn't exists.",
+          data: null,
+          success: false,
+        });
+      }
+    } else {
+      res.send({
+        message: "Cannot Delete Quiz.",
+        data: null,
+        success: false,
+      });
+    }
+  } catch (error) {
+    res.send({
+      message: error.message,
+      data: error,
+      success: false,
+    });
+  }
+};
+
+const addQuestionToQuiz = async (req, res) => {
+  try {
+    const user = await User.findById(req.body.userid);
+    if (user.isAdmin) {
+      // add question to Questions Collection
+      const newQuestion = new Question(req.body);
+      const question = await newQuestion.save();
+      // add question to quiz
+      const quiz = await Quiz.findOne({ _id: req.params.id });
+      quiz.questions.push(question._id);
+      await quiz.save();
+      res.send({
+        message: "Question added successfully.",
+        data: null,
+        success: true,
+      });
+    } else {
+      res.send({
+        message: "Question cannot be added.",
+        data: null,
+        success: false,
+      });
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.send({
+      message: error.message,
+      data: error,
+      success: false,
+    });
+  }
+};
+
+const editQuestionInQuiz = async (req, res) => {
+  try {
+    const user = await User.findById(req.body.userid);
+    if (user.isAdmin) {
+      await Question.findByIdAndUpdate(req.body.questionId, req.body);
+      res.send({
+        message: "Question edited successfully",
+        success: true,
+      });
+    } else {
+      res.send({
+        message: "Question cannot be edited.",
+        success: false,
+      });
+    }
+  } catch (error) {
+    res.send({
+      message: error.message,
+      data: error,
+      success: false,
+    });
+  }
+};
+
+const deleteQuestionFromQuiz = async (req, res) => {
+  try {
+    const user = await User.findById(req.body.userid);
+    if (user.isAdmin) {
+      // delete question from the questions collection
+      const question = await Question.findOne({ _id: req.body.questionId });
+      await question.delete();
+      // delete question in quiz
+      const quiz = await Quiz.findOne({ _id: req.params.id });
+      const questions = quiz.questions;
+      quiz.questions = questions.filter((question) => {
+        if (question._id != req.body.questionId) {
+          return question._id != req.body.questionId;
+        }
+      });
+      await quiz.save();
+      res.send({
+        message: "Selected question deleted successfully",
+        success: true,
+      });
+    } else {
+      res.send({
+        message: "Question cannot be deleted.",
+        success: false,
+      });
+    }
+  } catch (error) {
+    res.send({
+      message: error.message,
+      data: error,
+      success: false,
+    });
+  }
+};
+
+module.exports = {
+  addQuiz,
+  getAllQuizs,
+  getQuizById,
+  editQuiz,
+  deleteQuiz,
+  addQuestionToQuiz,
+  editQuestionInQuiz,
+  deleteQuestionFromQuiz,
+};
--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -4,6 +4,10 @@ const {
   getAllQuizs,
   getQuizById,
   editQuiz,
+  deleteQuiz,
+  addQuestionToQuiz,
+  editQuestionInQuiz,
+  deleteQuestionFromQuiz,
 } = require("../controllers/quizControllers");
 const authMiddleware = require("../middlewares/authMiddleware");
 
@@ -11,5 +15,13 @@ router.post("/addQuiz", authMiddleware, addQuiz);
 router.get("/getAllQuizs", authMiddleware, getAllQuizs);
 router.get("/getQuizById/:id", authMiddleware, getQuizById);
 router.put("/editQuiz/:id", authMiddleware, editQuiz);
+router.post("/addQuestionToQuiz/:id", authMiddleware, addQuestionToQuiz);
+router.put("/editQuestionInQuiz/:id", authMiddleware, editQuestionInQuiz);
+router.delete("/deleteQuiz/:id", authMiddleware, deleteQuiz);
+router.delete(
+  "/deleteQuestionFromQuiz/:id",
+  authMiddleware,
+  deleteQuestionFromQuiz
+);
 
 module.exports = router;
