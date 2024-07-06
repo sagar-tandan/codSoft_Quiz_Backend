4879a03602afb4843e3b3716ead533978696cd22
Sagar Tandan <076bct072@ioepc.edu.np>
2024-07-06 16:55:12 +0545

    Setting up the backend

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const db = require("./config/dbConfig");
+
+require("dotenv").config();
+const cors = require("cors");
+
+const logger = require("morgan");
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger("dev"));
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT: ${port}`);
+});
